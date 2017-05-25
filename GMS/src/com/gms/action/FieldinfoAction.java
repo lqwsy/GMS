927c9512eb0bafe74be1cd05cf2eff37c50a84e5
ringkin <1508057874@qq.com>
2017-05-25 19:03:08 +0800

    小修改

--- GMS/src/com/gms/action/FieldinfoAction.java
+++ GMS/src/com/gms/action/FieldinfoAction.java
@@ -1,7 +1,27 @@
 package com.gms.action;
 
+import com.gms.po.Fieldinfo;
+import com.gms.service.impl.FieldinfoServiceImpl;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class FieldinfoAction extends ActionSupport {
+	private Fieldinfo fieldInfo;
 
+	private FieldinfoServiceImpl fieldinfoService;
+	
+	public Fieldinfo getFieldInfo() {
+		return fieldInfo;
+	}
+
+	public void setFieldInfo(Fieldinfo fieldInfo) {
+		this.fieldInfo = fieldInfo;
+	}
+	
+	public void setFieldinfoService(FieldinfoServiceImpl fieldinfoService) {
+		this.fieldinfoService = fieldinfoService;
+	}
+	
+	public String getAllField() {
+		return SUCCESS;
+	}
 }
