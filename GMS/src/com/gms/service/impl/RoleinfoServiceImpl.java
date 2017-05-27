f25f4da05b70eab57f5cb16e2d43f0d1bd46421d
lqwsy <2567924198@qq.com>
2017-05-27 10:53:43 +0800

    更新Roleinfoservice的dao的get/set方法

--- GMS/src/com/gms/service/impl/RoleinfoServiceImpl.java
+++ GMS/src/com/gms/service/impl/RoleinfoServiceImpl.java
@@ -10,6 +10,14 @@ public class RoleinfoServiceImpl implements IRoleinfoService{
 
 	private IRoleinfoDao roledao;
 	
+	public IRoleinfoDao getRoledao() {
+		return roledao;
+	}
+
+	public void setRoledao(IRoleinfoDao roledao) {
+		this.roledao = roledao;
+	}
+
 	@Override
 	public Roleinfo getRoleinfoById(Integer iRoleId) {
 		return roledao.get(iRoleId);
